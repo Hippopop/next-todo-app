e4c3c1f4fd22c6c75ce8ccf127b76344881b8aac
Mostafijul Islam <mostafijul929@gmail.com>
2025-10-13 18:24:04 +0600

    chore: maintenance commit! (06:24 PM - 13 October, 25)

--- src/components/custom/TopNavigationBar.tsx
+++ src/components/custom/TopNavigationBar.tsx
@@ -2,14 +2,16 @@ import { ROUTES } from "@/lib/constants/paths";
 import { STRINGS } from "@/lib/constants/strings";
 
 import AppLogo from "./AppLogo";
+import ThemeSwitcher from "../providers/theme/ThemeSwitcher";
 
 export default function TopNavigationBar() {
     return (
-        <div className="fixed top-0 left-0 w-full z-10 flex justify-center gap-2 md:justify-start p-3 md:p-5">
+        <div className="fixed top-0 left-0 w-full z-10 flex justify-between items-center p-3 md:p-5">
             <a href={ROUTES.HOME} className="flex items-center gap-2 font-medium">
-                <AppLogo />
-                {STRINGS.APP_NAME}.
+            <AppLogo />
+            {STRINGS.APP_NAME}.
             </a>
+            <ThemeSwitcher />
         </div>
     );
 }
--- src/components/providers/theme/ThemeSwitcher.tsx
+++ src/components/providers/theme/ThemeSwitcher.tsx
@@ -2,21 +2,44 @@
 import { useTheme } from 'next-themes';
 import { $themeOptions } from './ThemeProvider';
 
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuLabel,
+    DropdownMenuRadioGroup,
+    DropdownMenuRadioItem,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/shadcn/dropdown-menu"
+import { Button } from '@/components/shadcn/button';
+
 const ThemeSwitcher = () => {
     const { theme, setTheme } = useTheme();
-    const themeIdx = $themeOptions?.indexOf(theme ?? 'light');
-    console.log(theme);
-    console.log(themeIdx);
-    const nextThemeIdx = (themeIdx + 1) % $themeOptions.length;
     return (
-        <button
-            onClick={() => {
-                setTheme($themeOptions[nextThemeIdx]);
-            }}
-        >
-            Switch to <span className="text-mostafij"> {$themeOptions[nextThemeIdx].toLocaleUpperCase()} </span>  mode
-        </button>
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <Button variant="outline">Theme</Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-56">
+                <DropdownMenuLabel>Choose Theme</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuRadioGroup value={theme} onValueChange={setTheme}>
+                    {
+                        $themeOptions.map(option =>
+                        (
+                            <DropdownMenuRadioItem key={option} value={option}>
+                                {option.toUpperCase()}
+                            </DropdownMenuRadioItem>
+                        )
+                        )
+                    }
+                </DropdownMenuRadioGroup>
+            </DropdownMenuContent>
+        </DropdownMenu>
     );
 };
+
+
+
 
 export default ThemeSwitcher;
